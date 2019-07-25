08aed037f8dc754fe16a79576c59f2712734c624
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2019-07-25 06:19:39 +0530

    Initial Commit

---
+++ DeepLabV3.py
@@ -0,0 +1,77 @@
+from tensorflow.keras.layers import (
+    DepthwiseConv2D,
+    BatchNormalization,
+    Lambda,
+    Conv2D,
+    ZeroPadding2D,
+    Activation
+)
+
+
+class DeepLabV3:
+
+    def __init__(self):
+        pass
+    
+
+    def sepconv_batchnorm(x, filters, prefix, stride = 1, kernel_size = 3, rate = 1, depth_activation = False, epsilon = 1e-3):
+        if stride == 1:
+            depth_padding = 'same'
+        else:
+            effective_kernel_size = kernel_size + (kernel_size - 1) * (rate - 1)
+            pad = effective_kernel_size - 1
+            pad_begin = pad // 2
+            pad_end = pad - pad_end
+            x = ZeroPadding2D((pad_begin, pad_end))(x)
+            depth_padding = 'valid'
+        
+        if not depth_activation:
+            x = Activation('relu')
+        
+
+        x = DepthwiseConv2D(
+            (
+                kernel_size,
+                kernel_size
+            ),
+            strides = (
+                stride,
+                stride
+            ),
+            dilation_rate = (
+                rate,
+                rate
+            ),
+            padding = depth_padding,
+            use_bias = False,
+            name = prefix + '_depthwise'
+        )
+
+        x = BatchNormalization(
+            name = prefix + '_depthwise_batch_norm',
+            epsilon = epsilon
+        )(x)
+
+        if depth_activation:
+            x = Activation('relu')(x)
+        
+        x = Conv2D(
+            filters,
+            (1, 1),
+            padding = 'same',
+            use_bias = False,
+            name = prefix + '_pointwise'
+        )
+
+        x = BatchNormalization(
+            name = prefix + '_pointwise_batch_norm',
+            epsilon = epsilon
+        )(x)
+
+        if depth_activation:
+            x = Activation('relu')
+        
+        return x
+    
+
+    
